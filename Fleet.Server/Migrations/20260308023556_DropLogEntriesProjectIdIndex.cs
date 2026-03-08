986060329bf5ba4c84dbeee989b676f50dc69763
Rusheel <rusheel@caredocs.ai>
2026-03-07 18:38:53 -0800

    feat: Add migration to drop ProjectId index from LogEntries table

---
+++ Fleet.Server/Migrations/20260308023556_DropLogEntriesProjectIdIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fleet.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropLogEntriesProjectIdIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_LogEntries_ProjectId",
+                table: "LogEntries");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_LogEntries_ProjectId",
+                table: "LogEntries",
+                column: "ProjectId");
+        }
+    }
+}
